9469d9f1ed857ecaa3d1822f4de1a771459ee8b3
Raoni <raonig3@gmai.com.br>
2026-08-03 14:54:18 -0300

    ajuste 11 no start do node

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -18,9 +18,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: [
-      '://hawkstecnologia.com.br'
-    ],
+    allowedHosts: true,
     proxy: {
       "/api": {
         target: "http://127.0.0.1:3333",
@@ -34,6 +32,7 @@ export default defineConfig({
   },
   preview: {
     port: 4173,
+    allowedHosts: true,
     proxy: {
       "/api": {
         target: "http://127.0.0.1:3333",
